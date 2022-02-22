3189ba64327b7dfc895a5be8e90eea945dae6c53
Hariom Sharma <punits152@gmail.com>
2022-02-23 01:15:15 +0530

    Uploads

--- site_pages/urls.py
+++ site_pages/urls.py
@@ -5,4 +5,5 @@ from .views import page
 urlpatterns = (
 re_path(r'^(?P<slug>[\w./-]+)/$', page, name='page'),
 re_path(r'^$', page, name='homepage'),
+re_path('index', page, name='homepage_right')
 )
